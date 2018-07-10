f70ab763cf03f739b950d4adf3795ecf3e76ac34
Alex <707279282@qq.com>
2018-07-10 18:05:12 +0800

    add study day-07

---
+++ www/config_default.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__author__ = AL
+__Link__ = 707279282@qq.com
+__mtime__ = 2018/7/10 9:38
+__version__ = python3.6
+
+"""
---
+++ www/coroweb.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+__author__ = AL
+__Link__ = 707279282@qq.com
+__mtime__ = 2018/7/9 14:33
+__version__ = python3.6
+
+"""
